54edb72cae987ca6640b62321ea55c9ef4aee793
Carlos Alexandre Lopes <carlosalopes@outlook.com.br>
2023-10-27 17:03:18 -0300

    inicio do desenvolvimento

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 require("dotenv").config()
 
 const express = require('express')
-const app     = express()
+const cors    = require('cors')
 
+const router  = require('./src/routes/routes')
 const port = process.env.PORT
 
-app.listen(port,() => {
-  console.log(port)
+const app     = express()
+app.use(cors())
+app.use(express.json())
+app.use(router)
+
+app.listen(port,() => {})
+
+app.get('/',(request,response)=>{
+  response.send("Hello world")
 })
---
+++ src/controllers/UsuarioController.js
@@ -0,0 +1,9 @@
+const database = require('../database/connection')
+
+class UsuarioController {
+  novoUsuario(request,response){
+    
+  }
+}
+
+module.exports = new UsuarioController()
